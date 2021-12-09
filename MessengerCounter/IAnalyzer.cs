4f21b2f27b385c23775766429b54a8f089c10d36
Jakub Rumpel <jakub@life-coder.pl>
2021-12-09 16:17:14 +0100

    Added IAnalyzer for operations on messages

---
+++ MessengerCounter/IAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿namespace MessengerCounter
+{
+    interface IAnalyzer
+    {
+        public IAnalyzer CreateInstance(string conversationName, string inputPath, string outputPath);
+        
+        public string ConversationName { get; set; }
+        public string InputPath { get; set; }
+        public string OutputPath { get; set; }
+        
+        public void GetMessages();
+        public void Analyze();
+        public void PrettyPrint();
+        public void SaveOutput();
+    }
+}
